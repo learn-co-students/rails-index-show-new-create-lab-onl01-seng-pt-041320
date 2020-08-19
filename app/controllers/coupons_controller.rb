6e488330e42106f5fd29f295272ae9e72e41556e
OrkunSA <c.orkun.s@gmail.com>
2020-08-19 13:47:27 -0500

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -24,7 +24,7 @@ class CouponsController < ApplicationController
     @coupon.coupon_code = params[:coupon][:coupon_code]
     @coupon.store = params[:coupon][:store]
     @coupon.save
-    redirect_to coupon_path(@coupon.id)
+    redirect_to coupon_path(@coupon)
 end
 
 
