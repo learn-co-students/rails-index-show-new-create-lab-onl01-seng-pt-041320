8cb76f877dfcbf05c683a1ce8f4be18898c5f790
OrkunSA <c.orkun.s@gmail.com>
2020-08-19 13:44:54 -0500

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,41 @@
+class CouponsController < ApplicationController
+  before_action :set_coupon, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @coupons = Coupon.all
+  end
+
+ 
+  def show
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+ 
+  def edit
+  end
+
+  # POST /coupons
+  # POST /coupons.json
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon][:coupon_code]
+    @coupon.store = params[:coupon][:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon.id)
+end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_coupon
+      @coupon = Coupon.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def coupon_params
+      params.require(:coupon).permit(:coupon_code, :store)
+    end
+end
